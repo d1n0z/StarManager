07c0ec4a8f2510c4c9c17f17c521d4d1d60b47c9
ITsholi <ilushkaplyshka@gmail.com>
2025-03-06 16:51:14 +0400

    ⠀

--- Bot/comment_handlers.py
+++ Bot/comment_handlers.py
@@ -10,10 +10,9 @@ from db import pool
 
 async def comment_handle(event: GroupTypes.WallReplyNew) -> None:
     uid = event.object.from_id
+    if uid <= 0:
+        return
     if event.object.post_id == FARM_POST_ID:
-        if uid <= 0:
-            return
-
         async with (await pool()).connection() as conn:
             async with conn.cursor() as c:
                 if not (com := await (await c.execute('select id, time from comments where uid=%s', (uid,))).fetchone()):
@@ -41,10 +40,7 @@ async def comment_handle(event: GroupTypes.WallReplyNew) -> None:
                         message=messages.newpost_dup(await getUserName(uid), uid), reply_to_comment=event.object.id)
                 await c.execute('insert into newpostcomments (uid, pid) values (%s, %s)', (uid, event.object.post_id))
                 await conn.commit()
+        await addUserXP(uid, 250)
         return await api.wall.create_comment(
             owner_id=-GROUP_ID, post_id=event.object.post_id, from_group=GROUP_ID,
-            message=messages.newpost_dup(await getUserName(uid), uid), reply_to_comment=event.object.id)
-    else:
-        return await api.wall.create_comment(
-            owner_id=-GROUP_ID, post_id=event.object.post_id, from_group=GROUP_ID,
-            message=messages.newpost_toolate(await getUserName(uid), uid), reply_to_comment=event.object.id)
+            message=messages.newpost(await getUserName(uid), uid), reply_to_comment=event.object.id)
