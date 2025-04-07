3255e966779eb915e0144e934ec6905af7894d94
ITsholi <ilushkaplyshka@gmail.com>
2025-04-07 16:39:12 +0400

    ⠀

--- db.py
+++ db.py
@@ -4,6 +4,9 @@ from psycopg_pool import ConnectionPool
 from config.config import DATABASE_STR
 
 
+async def init_connection(conn):
+    await conn.execute("SET idle_in_transaction_session_timeout = '15s'")
+
 _pool = None
 _syncpool = None
 
@@ -12,7 +15,7 @@ async def pool():
     global _pool
     if _pool is None:
         _pool = await create_asyncpool(DATABASE_STR, min_size=5, max_size=80, max_inactive_connection_lifetime=30,
-                                       timeout=30, command_timeout=60)
+                                       timeout=30, command_timeout=60, init=init_connection)
     return _pool
 
 
