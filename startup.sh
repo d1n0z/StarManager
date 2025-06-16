9f919bec440956968808370df7e5f4d42ac0dd63
ITsholi <ilushkaplyshka@gmail.com>
2025-06-16 19:51:23 +0400

    fixed startup

--- startup.sh
+++ startup.sh
@@ -4,3 +4,6 @@ pkill python
 
 tmux new -s bot -d
 tmux send-keys -t bot 'cd /root/StarManager/' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'python3.11 main.py' ENTER
+
+tmux new -s site -d
+tmux send-keys -t site 'cd /root/StarManager/site' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'fastapi run main.py --host 127.0.0.1 --port 5000' EN>
