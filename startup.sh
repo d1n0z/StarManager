9a62ab01fd8d7044716a9d9883f7dc144db3e3d9
ITsholi <ilushkaplyshka@gmail.com>
2025-07-30 12:32:19 +0400

    startup optimisation

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pkill python
-
+tmux kill-session -t bot
 tmux new -s bot -d
 tmux send-keys -t bot 'cd /root/StarManager/' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'python3.11 main.py' ENTER
 
+tmux kill-session -t site
 tmux new -s site -d
 tmux send-keys -t site 'cd /root/StarManager/site' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'fastapi run main.py --host 127.0.0.1 --port 5000' ENTER
