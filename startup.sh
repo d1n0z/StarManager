140252e621930bf0d65cb151e5962b6bd1787635
d1n0tf <ilushkaplyshka@gmail.com>
2025-08-25 17:50:00 +0400

    edit path

--- startup.sh
+++ startup.sh
@@ -2,8 +2,8 @@
 
 tmux kill-session -t bot
 tmux new -s bot -d
-tmux send-keys -t bot 'cd /root/StarManager/' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'python3.11 main.py' ENTER
+tmux send-keys -t bot 'cd /opt/StarManager/' ENTER '. /opt/StarManager/venv/bin/activate' ENTER 'python3.11 main.py' ENTER
 
 tmux kill-session -t site
 tmux new -s site -d
-tmux send-keys -t site 'cd /root/StarManager/site' ENTER '. /root/StarManager/venv/bin/activate' ENTER 'fastapi run main.py --host 127.0.0.1 --port 5000' ENTER
+tmux send-keys -t site 'cd /opt/StarManager/site' ENTER '. /opt/StarManager/venv/bin/activate' ENTER 'fastapi run main.py --host 127.0.0.1 --port 5000' ENTER
