740ae3dddb06cd081c63deec5202a65a3b73f914
d1n0tf <ilushkaplyshka@gmail.com>
2025-08-28 15:00:38 +0400

    hotfix

--- src/StarManager/vkbot/handlers/payload_handlers.py
+++ src/StarManager/vkbot/handlers/payload_handlers.py
@@ -1346,8 +1346,8 @@ async def top(message: MessageEvent):
     chat_id = peer_id - 2000000000
     async with (await pool()).acquire() as conn:
         res = await conn.fetch(
-            "select uid, await messages from await messages where uid>0 and await messages>0 and chat_id=$1 and "
-            "uid=ANY($2) order by await messages desc limit 10",
+            "select uid, messages from messages where uid>0 and await messages>0 and chat_id=$1 and "
+            "uid=ANY($2) order by messages desc limit 10",
             chat_id,
             [
                 i.member_id
