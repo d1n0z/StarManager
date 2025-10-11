e46fbf4dac28ad5014c63abffe862478773d2d88
d1n0tf <ilushkaplyshka@gmail.com>
2025-10-12 01:50:11 +0400

    diagnose

--- src/StarManager/vkbot/message_handlers.py
+++ src/StarManager/vkbot/message_handlers.py
@@ -66,12 +66,14 @@ async def message_handle(event: MessageNew) -> Any:
                 kbd=keyboard.pm_market(event.object.message.from_id),
             )
     
+    print(11)
     uid = event.object.message.from_id
     msgtime = event.object.message.date
     chat_id = event.object.message.peer_id - 2000000000
     chat_settings = await get_chat_settings(chat_id)
     uacc = await get_user_access_level(uid, chat_id) if uid > 0 else 0
 
+    print(12)
     if chat_settings["main"]["nightmode"] and uacc <= 6:
         async with (await pool()).acquire() as conn:
             chatsetting = await conn.fetchrow(
@@ -108,6 +110,7 @@ async def message_handle(event: MessageNew) -> Any:
     if uid in settings.service.admins:
         print(f"{uid}({chat_id}): {msg}")
 
+    print(13)
     filterdata, pnt = msg.lower().replace(" ", ""), -1
     async with (await pool()).acquire() as conn:
         await conn.execute(
@@ -258,6 +261,7 @@ async def message_handle(event: MessageNew) -> Any:
             ),
         )
 
+    print(14)
     data = event.object.message.text.split()
     if not any(
         event.object.message.text.startswith(i)
@@ -388,6 +392,7 @@ async def message_handle(event: MessageNew) -> Any:
     except Exception:
         sticker = False
 
+    print(15)
     if chat_settings["antispam"]["messagesPerMinute"]:
         async with (await pool()).acquire() as conn:
             if await conn.fetchval(
@@ -457,4 +462,6 @@ async def message_handle(event: MessageNew) -> Any:
                         math[4],
                     ),
                 )
+    print(16)
     await add_msg_counter(chat_id, uid, audio, sticker)
+    print(17)
